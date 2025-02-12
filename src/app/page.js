09f5f305f7d6bebc4bb1c302d8fe22fd15dd0bb7
Adithya M R <adithya@thinkvidya.com>
2025-02-13 01:11:46 +0530

    canvas

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,17 @@
+import Canvas from "@/components/canvas";
 import { DownArrow } from "@/components/downArrow";
 import NameBanner from "@/components/name";
 import NavBar from "@/components/navBar";
 
 export default function Page() {
   return (
-    <section className="flex flex-col justify-between p-16 items-center h-screen">
-      <NavBar />
-      <NameBanner />
-      <DownArrow />
-    </section>
+    <>
+      <section className="flex flex-col justify-between p-10 items-center h-screen">
+        <NavBar />
+        <NameBanner />
+        <DownArrow />
+        <Canvas />
+      </section>
+    </>
   );
 }
